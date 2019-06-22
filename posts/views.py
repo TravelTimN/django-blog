db8ee13ed7a3f9ebf82c9dfe0f785ca3f8d1ce27
Tim Nelson <traveltimn@gmail.com>
2019-06-22 17:41:46 +0100

    Update blog miniproject

--- posts/views.py
+++ posts/views.py
@@ -9,7 +9,7 @@ def get_posts(request):
     Create a view that will return a list of Posts that were published
     prior to .now and render them to the 'blogposts.html' template.
     """
-    posts = Post.object.filter(
+    posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by("-published_date")
     return render(request, "blogposts.html", {"posts": posts})
 
